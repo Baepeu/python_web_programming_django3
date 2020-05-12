121b3c9b51c750d8f9452af288cd4dfa12bb5300
Beapeu <beap.jg@gmail.com>
2020-05-12 20:24:36 +0900

    final

--- 04_bookmark/bookmark/urls.py
+++ 04_bookmark/bookmark/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("add/", BookmarkCreateView.as_view(), name='add'),
     path("detail/<int:pk>/", BookmarkDetailView.as_view(), name='detail'),
     path("update/<int:pk>/", BookmarkUpdateView.as_view(), name='update'),
+    path("delete/<int:pk>/", BookmarkDeleteView.as_view(), name='delete'),
 ]
