816eb025b7a8a01ba1455679e2731c63d6c22dc5
Beapeu <beap.jg@gmail.com>
2020-07-19 23:37:48 +0900

    transaction update

--- 06_onlineshop/order/iamport.py
+++ 06_onlineshop/order/iamport.py
@@ -16,7 +16,45 @@ def get_token():
         return None
 
 def payments_prepare(order_id, amount, *args, **kwargs):
-    pass
+    access_token = get_token()
+    if access_token:
+        access_data = {
+            'merchant_uid':order_id,
+            'amount':amount
+        }
+        url = "https://api.iamport.kr/payments/prepare"
+        headers = {
+            'Authorization':access_token
+        }
+        req = requests.post(url, data=access_data, headers=headers)
+        res = req.json()
+        if res['code'] != 0:
+            raise ValueError("API 통신 오류")
+    else:
+        raise ValueError("토큰 오류")
+
+
 
 def find_transaction(order_id, *args, **kwargs):
-    pass
+    access_token = get_token()
+    if access_token:
+        url = "https://api.iamport.kr/payments/find/"+order_id
+        headers = {
+            'Authorization':access_token
+        }
+        req = requests.post(url, headers=headers)
+        res = req.json()
+        if res['code'] == 0:
+            context = {
+                'imp_id':res['response']['imp_uid'],
+                'merchant_order_id':res['response']['merchant_uid'],
+                'amount':res['response']['amount'],
+                'status':res['response']['status'],
+                'type':res['response']['pay_method'],
+                'receipt_url':res['response']['receipt_url']
+            }
+            return context
+        else:
+            return None
+    else:
+        raise ValueError("토큰 오류")
--- 06_onlineshop/order/models.py
+++ 06_onlineshop/order/models.py
@@ -1,3 +1,89 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
+from shop.models import Product
+from coupon.models import Coupon
 # Create your models here.
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    coupon = models.ForeignKey(Coupon, on_delete=models.PROTECT, related_name='order_coupon', null=True, blank=True)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100000)])
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    def get_total_product(self):
+        return sum(item.get_item_price() for item in self.items.all())
+
+    def get_total_price(self):
+        total_product = self.get_total_product()
+        return total_product - self.discount
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='order_products')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_item_price(self):
+        return self.price * self.quantity
+
+import hashlib
+from .iamport import payments_prepare, find_transaction
+class OrderTransactionManager(models.Manager):
+    def create_new(self, order, amount, success=None, transaction_status=None):
+        if not order:
+            raise ValueError("주문 정보 오류")
+
+        order_hash = hashlib.sha1(str(order.id).encode('utf-8')).hexdigest()
+        email_hash = str(order.email).split("@")[0]
+        final_hash = hashlib.sha1((order_hash+email_hash).encode('utf-8')).hexdigest()[:10]
+        merchant_order_id = str(final_hash) # 동영상 강의에서 변경
+
+        payments_prepare(merchant_order_id, amount)
+
+        transaction = self.model(
+            order=order,
+            merchant_order_id=merchant_order_id,
+            amount=amount
+        )
+
+        if success is not None:
+            transaction.success = success
+            transaction.transaction_status = transaction_status
+
+        try:
+            transaction.save()
+        except Exception as e:
+            print("save error", e)
+
+        return transaction.merchant_order_id
+
+    def get_transaction(self, merchant_order_id):
+        result = find_transaction(merchant_order_id)
+        if result['status'] == 'paid':
+            return result
+        else:
+            return None
+
+class OrderTransaction(models.Model):
+    pass
+
+
+
